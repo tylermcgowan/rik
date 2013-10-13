aad6eca7f6d8e18abe0bcf23397fb32ead46c91a
Tyler McGowan <tyler@cedarmade.co>
2013-10-13 21:15:38 +0100

    Added Project template to test how it works, and first attempt at formatting a single project. Added a custom blueprint for the single project to have the intro text field in the panel.

---
+++ site/templates/project.php
@@ -0,0 +1,15 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+<?php snippet('submenu') ?>
+
+<section class="content">
+
+  <article>
+    <h1><?php echo html($page->title()) ?></h1>
+    <?php echo kirbytext($page->intro()) ?>
+    <?php echo kirbytext($page->text()) ?>
+  </article>
+
+</section>
+
+<?php snippet('footer') ?>
