4cb469172cfbcf620a78c2e20e9be76d57857c1e
Tyler McGowan <tyler@cedarmade.co>
2013-10-16 14:49:50 +0100

    Added projectlist snippet to list the projects on the hompepage, since submenu is different. Also added the rik snippet so the logo and tagline can be used as a module.

---
+++ site/snippets/rik.php
@@ -0,0 +1,5 @@
+<section class="rik" role="branding">
+	<h1>
+		<a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/logo.png') ?>" width="115" height="41" alt="<?php echo html($site->title()) ?>" /></a>
+	</h1>
+</section>
