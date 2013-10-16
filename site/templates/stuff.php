de8849dceb5a9071663302398346013b61fe4544
Tyler McGowan <tyler@cedarmade.co>
2013-10-16 09:47:35 +0100

    Created template file for the Stuff page to list all the projects.

---
+++ site/templates/stuff.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+<?php snippet('menu') ?>
+
+<section class="content">
+
+  <article>
+  	<?php snippet('submenu') ?>
+  </article>
+
+</section>
+
+<?php snippet('footer') ?>
