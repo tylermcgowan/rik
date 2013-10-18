f5718693e98499b2b5b6b74f1f7b0e5bee3ad240
Tyler McGowan <tyler@cedarmade.co>
2013-10-18 23:28:23 +0100

    Don't think anything changed.

--- site/snippets/projectlist.php
+++ site/snippets/projectlist.php
@@ -1,5 +1,5 @@
 <nav class="projects">
 	<?php foreach($pages->children() as $child): ?>			
-		<a<?php echo ($child->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $child->url() ?>"><?php echo $child->title() ?></a>	
+		<a<?php echo ($child->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $child->url() ?>"><?php echo $child->title() ?></a><hr>
 	<?php endforeach ?>
 </nav>
