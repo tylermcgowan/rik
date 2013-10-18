853ba1d7ffce96cad040fc1bf0e203a8f66ac7a2
Tyler McGowan <tyler@cedarmade.co>
2013-10-18 23:37:28 +0100

    Wrapped project content in HTML container and row to contain and style. Added project list and nav below in proper markup like the hompeage/stuff.

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,17 +1,27 @@
 <?php snippet('header') ?>
 
-<section class="content">
-  <article>
-	  <h3 class="project-title"><?php echo html($page->title()) ?></h3>
-    <h2 class="intro"><?php echo kirbytext($page->intro()) ?></h2>
-    <?php echo kirbytext($page->text()) ?>
-  </article>
-</section>
+<div class="container">
+	<div class="row">
+		<section class="content">
+			<article>
+				<h3 class="project-title"><?php echo html($page->title()) ?></h3>
+				<h2 class="intro"><?php echo html($page->intro()) ?></h2>
+				<?php echo kirbytext($page->text()) ?>
+			</article>
+		</section>
+	</div>
+</div>
 
-<section class="projectlist">
-	<?php snippet('menu') ?>
-	<?php snippet('rik') ?>	
-	<?php snippet('submenu') ?>
-</section>
+<div class="gradient">
+	<div class="container">
+		<div class="row">
+			<?php snippet('menu') ?>
 
-<?php snippet('footer') ?>
+			<?php snippet('rik') ?>
+			
+			<?php snippet('submenu') ?>
+			
+			<?php snippet('footer') ?>
+		</div>
+	</div>
+</div>
