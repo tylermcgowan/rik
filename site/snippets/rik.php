8e32d9d6c53663aa403e53dc93d30e6f104a3da4
Tyler McGowan <tyler@cedarmade.co>
2013-10-18 23:30:26 +0100

    Added the tagline in the rik snippet and decreased the logo size.

--- site/snippets/rik.php
+++ site/snippets/rik.php
@@ -1,5 +1,6 @@
 <section class="rik" role="branding">
-	<h1>
-		<a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/logo.png') ?>" width="115" height="41" alt="<?php echo html($site->title()) ?>" /></a>
-	</h1>
+	<h1><a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/rik-white.png') ?>" width="74" height="45" alt="<?php echo html($site->title()) ?>" /></a></h1>
+	<h2 class="tagline">
+		copywriting for the creative arts
+	</h2>
 </section>
